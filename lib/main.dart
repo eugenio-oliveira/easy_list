c6147f5b9a55566d6c7f9a4cc46dc81580652e43
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2019-09-03 16:32:19 -0300

    cleaning syntax

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 import 'ui/home_page.dart';
 
-void main(){
-  runApp(MaterialApp(
-    home: Home(),
-    debugShowCheckedModeBanner: false,
-  ));
-}
 
-
+void main() => runApp(HomePage());
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+    title: "Easy List 1.0",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Easy List"),
+        ),
+        body: Center(
+          child: Text("Soon..."),
+        ),
+      ),
+    );
   }
 }
 
